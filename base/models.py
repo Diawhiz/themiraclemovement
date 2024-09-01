661a0d3b660571fa2a5ba4538d3fdf6a58cf4408
Ariyo Israel Gbemisola <igbemisola53@gmail.com>
2024-09-01 10:27:21 +0100

    created form model for contact us feature and first timers form

--- base/models.py
+++ base/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
 # Create your models here.
+
+class Contacts(models.Model):
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['created_on']
+
+    def __str__(self):
+        return self.name
+
+
+class FirstTimer(models.Model):
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    phone = models.IntegerField()
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['created_on']
+
+    def __str__(self):
+        return self.name
